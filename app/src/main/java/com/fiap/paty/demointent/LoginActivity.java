2fefdd9d0d448fa320d999fb7622549b56809c4b
patyalves17 <Patyptrc2915>
2017-06-07 22:39:18 -0300

    bradcast

--- app/src/main/java/com/fiap/paty/demointent/LoginActivity.java
+++ app/src/main/java/com/fiap/paty/demointent/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.fiap.paty.demointent;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -24,6 +28,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        requestSmsPermission();
     }
 
     @OnClick(R.id.novo)
@@ -42,5 +47,17 @@ public class LoginActivity extends AppCompatActivity {
                 break;
         }
 
+
     }
+
+    private void requestSmsPermission() {
+        String permission = Manifest.permission.RECEIVE_SMS;
+        int grant = ContextCompat.checkSelfPermission(this, permission);
+        if ( grant != PackageManager.PERMISSION_GRANTED) {
+            String[] permission_list = new String[1];
+            permission_list[0] = permission;
+            ActivityCompat.requestPermissions(this, permission_list, 1);
+        }
+    }
+
 }
